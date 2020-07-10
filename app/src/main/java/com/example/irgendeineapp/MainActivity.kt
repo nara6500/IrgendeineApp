748796e70198dc62f88e01b5662daaf3d5cc6d16
nara6500 <n-mpg@web.de>
2020-07-10 12:16:09 +0200

    TestButton, Try to Run

--- app/src/main/java/com/example/irgendeineapp/MainActivity.kt
+++ app/src/main/java/com/example/irgendeineapp/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.irgendeineapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Hello there.")
-        println("Hello back!")
+
+        chats_button.setOnClickListener {
+            val intent = Intent(this, MessagesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/irgendeineapp/MessagesActivity.kt
@@ -0,0 +1,15 @@
+package com.example.irgendeineapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MessagesActivity: AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messages)
+
+
+    }
+}
