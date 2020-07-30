ab16657768f9f730c50dd8a7b7cafceee96c1304
nara6500 <n-mpg@web.de>
2020-07-30 16:30:20 +0200

    add groupie, picasso and firebase

--- app/src/main/java/com/example/irgendeineapp/MainActivity.kt
+++ app/src/main/java/com/example/irgendeineapp/MainActivity.kt
@@ -5,25 +5,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.message_button.*
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        chat_List.layoutManager = LinearLayoutManager(this)
-        chat_List.adapter = MyCustomAdapter()
+
+        val adapter = GroupAdapter<GroupieViewHolder>()
 
 
+        adapter.add(UserItem())
 
-
-
+        chat_List.adapter = adapter
 
     }
 }
 
+class UserItem: Item<GroupieViewHolder>(){
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        // will be called in our list for each user objject later on...
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_message
+    }
+}
 
 
